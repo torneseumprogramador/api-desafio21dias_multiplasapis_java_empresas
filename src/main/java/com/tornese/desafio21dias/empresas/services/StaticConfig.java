b53bd143dd748c09e61bd0aaf7e9447ffcdc8744
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2021-12-08 20:28:14 -0300

    cors

--- src/main/java/com/tornese/desafio21dias/empresas/services/StaticConfig.java
+++ src/main/java/com/tornese/desafio21dias/empresas/services/StaticConfig.java
@@ -1,6 +1,7 @@
 package com.tornese.desafio21dias.empresas.services;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,5 +12,12 @@ public class StaticConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/api/**").addResourceLocations("classpath:/api/");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
 }
